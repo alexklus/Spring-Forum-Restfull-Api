57e81a2892bcd426c3614cf85aa15c4d3906b7ca
alexklus <55231756+alexklus@users.noreply.github.com>
2021-04-01 08:43:35 +0300

    Update db (2).sql

--- sql/db (2).sql
+++ sql/db (2).sql
@@ -1,3 +1,21 @@
+-- Database: forum
+
+-- DROP DATABASE forum;
+
+CREATE DATABASE forum
+    WITH 
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'English_United States.1252'
+    LC_CTYPE = 'English_United States.1252'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
+
+
+
+CREATE SCHEMA forumdb;
+
+
 
 -- Table: public.users
 
